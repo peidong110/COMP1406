11c336e6a4c9ea8d3f66e9b40bd18e67bf1c3b54
Payton Dong Pei <silence1198@outlook.com>
2018-05-03 03:11:21 -0400

    Create Patron.java

---
+++ ASSIGNMENT2/Patron.java
@@ -0,0 +1,14 @@
+public class Patron {
+    public int age;
+
+    Ticket ticket = null;
+    public String currentMovie = null;
+    public Patron(){
+        age = 0;
+
+    }
+    public Patron(int age){
+        this.age = age;
+    }
+
+}
